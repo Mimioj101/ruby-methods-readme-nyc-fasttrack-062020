3968f461ca20db42390fb7bade363427f266ebad
reactive-source-2578 <mimioj@gmail.com>
2020-06-22 17:41:41 +0000

    Automatically backed up by Learn

--- greeting.rb
+++ greeting.rb
@@ -8,18 +8,3 @@ greeting
 greeting
 greeting
 greeting
-
-
-# Save your file and run it with `ruby greeting.rb`. You'll see:
-
-# ```bash
-# $ ruby greeting.rb
-# Hello World
-# Hello World
-# Hello World
-# Hello World
-# Hello World
-# $
-# ```
-
-# The bareword `greeting` will execute the body of the defined method.
