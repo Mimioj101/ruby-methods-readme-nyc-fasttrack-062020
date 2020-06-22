d40283c4e905eaaa0f961acc405b72b64798ea0f
reactive-source-2578 <mimioj@gmail.com>
2020-06-22 17:39:30 +0000

    Automatically backed up by Learn

--- greeting.rb
+++ greeting.rb
@@ -8,3 +8,78 @@ greeting
 greeting
 greeting
 greeting
+
+
+
+Let's try making a method we can use over and over. Make a new file called
+`greeting.rb` (you can use: `touch greeting.rb` from your terminal). Put the
+following code in it:
+
+File: `greeting.rb`
+
+```ruby
+def greeting
+  puts "Hello World"
+end
+```
+
+Save your file and run it with `ruby greeting.rb`. You'll see:
+
+```bash
+$ ruby greeting.rb
+$
+```
+
+You'll notice that when you run your program, nothing happens. Your program
+successfully defined the method but it never executed it. Just because you built
+a machine doesn't mean that you turned it on. Update your `greeting.rb` to
+entirely read:
+
+File: `greeting.rb`
+
+```ruby
+def greeting
+  puts "Hello World"
+end
+
+greeting
+```
+
+Save your file and run it with `ruby greeting.rb`. You'll see:
+
+```bash
+$ ruby greeting.rb
+Hello World
+$
+```
+
+Now your program actually executed the program. Update the code again to
+entirely read:
+
+File: `greeting.rb`
+
+```ruby
+def greeting
+  puts "Hello World"
+end
+
+greeting
+greeting
+greeting
+greeting
+greeting
+```
+
+Save your file and run it with `ruby greeting.rb`. You'll see:
+
+```bash
+$ ruby greeting.rb
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+$
+```
+
+The bareword `greeting` will execute the body of the defined method.
